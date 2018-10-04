25737cf57b3c581fc687423c1dbbc8bcfaab7656
qscgy <sam.ehrenstein@gmail.com>
2018-10-04 15:14:56 -0400

    add script to convert txt to npy

--- MC_full3a.py
+++ MC_full3a.py
@@ -2,6 +2,7 @@ import numpy as np
 import scipy.stats as ss
 import random
 import subprocess as sp
+import sys
 
 
 #   |   ||
@@ -30,20 +31,23 @@ else:
     call_crp = "./"+call_crp
 
 evt_id = np.loadtxt(infile, usecols=(3,), dtype=np.float64)
-E, glon, glat = np.loadtxt(infile, usecols=(0, 1, 2), unpack=True)  # TODO cols should be 5, 1, 2
+glat, glon, E = np.loadtxt(infile, usecols=(0, 1, 2), unpack=True)  # TODO cols should be 5, 1, 2
+n_its = int(sys.argv[1])    # number of iterations
 
-for i in range(1):  # TODO change back to 10
+for i in range(evt_id.shape[0]):  # TODO change back to 10
     ID = evt_id[i]
     outfile = 'samples/evt_%i_a.txt' % ID
+    np_outfile = 'samples/evt_%i_a.npy' % ID
     if docker:
         outfile = "cosmicrays/"+outfile
+        np_outfile = 'cosmicrays/'+outfile
 
-    data_array = np.zeros((100, 9))     # TODO change back to (20000, 9)
+    data_array = np.zeros((n_its, 9))     # TODO change back to (20000, 9)
 
     for j in range(data_array.shape[0]):
         print("Iteration %i / %i" % (j, data_array.shape[0]))
         if j % 50 == 0:
-            np.savetxt(outfile % ID, data_array)  # Save to disk every 1000 iterations
+            np.savetxt(outfile % ID, data_array)  # Save to disk every 50 iterations
         s1, s2, s3 = random.sample(range(1, 2 ** 26 - 1), 3)
         # print('{0} {1} {2}'.format(s1, s2, s3))
         cmd = ['timeout', '35', call_crp, '%f' % E[i],
@@ -68,4 +72,5 @@ for i in range(1):  # TODO change back to 10
             print("Timeout")
         # print(out)
         # print(type(data_array[j, 0]))
+    np.save(np_outfile, data_array)
     np.savetxt(outfile, data_array)
--- plot_events.py
+++ plot_events.py
@@ -2,42 +2,132 @@ import numpy as np
 import pandas as pd
 from mpl_toolkits.basemap import Basemap
 import matplotlib.pyplot as plt
+from crpropa import *
+from numpy.random import random
 
-df = pd.read_table('/home/sam/Downloads/auger-data/Archive', sep='\s+', skiprows=7)
-df.fillna(0.0, inplace=True)
 
-data = df.values
-data_t = df.values.transpose()
-# print(data_t.shape)
+# Make maps and stuff of Auger data.
 
-nmost = 2000
-largest = np.argpartition(data_t[4], -nmost)[-nmost:]
-print(largest)
-# plt.figure()
-energies = data_t[4, largest]
-# h = np.histogram(energies, bins=20)
-mask = np.ma.array(largest, mask=False)
-print(mask)
-for i in range(largest.shape[0]):
-    # filter out abnormally large values or values where missing values had to be filled in with 0
-    if energies[i] > 500 or abs(data_t[38, largest[i]]) < 0.000001:
-        mask[i] = np.ma.masked
-largest = mask.compressed()
-print(largest)
-energies = data_t[4, largest]
-print(energies)
-print(np.count_nonzero(energies == 0.192373))
-l = data_t[5, largest]
-b = data_t[6, largest]
-# plt.hist(energies, bins=200)
-# plt.show()
 
+# AugerId
+# Number of Selected Stations
+# Local Theta
+# Local Phi
+# Energy
+# L
+# B
+# UTC Time
+# TCore
+# XCore
+# YCore
+# S1000
+# dS1000
+# Ra
+# Dec
+# Theta (should not be used)
+# Phi (should not be used)
+# dTheta
+# dPhi
+# dXCore
+# dYCore
+# Etimation and Reconstruction compatibles
+# Is T5
+# Is T5+
+# Is T5++
+# Is ICRC2005
+# Is Fd trigger
+# Geometric fit Chi2
+# LDF fit Chi2
+# Global fit Chi2
+# Global fit Ndof
+# LDF Beta
+# LDF Gamma
+# R
+# OldId
+# IsICRC2005Posterior
+# Icrc Energy
+# Delta Omega
+# Delta Core
+
+
+def get_useful_info(event):
+    print("Multiplicity: {0} Energy: {1} Flag 22: {2} Flag 23: {3}".format(event[1], event[4], event[21], event[22]))
+    print(event)
+
+
+def process_raw_auger(fname, sep, skiprows):
+    df = pd.read_table(fname, sep=sep, skiprows=skiprows)
+    df.fillna(0.0, inplace=True)
+
+    data = df.values
+    data_t = df.values.transpose()
+    # print(data_t.shape)
+
+    nmost = 2000
+    largest = np.argpartition(data_t[4], -nmost)[-nmost:]
+    # print(largest)
+    # plt.figure()
+    energies = data_t[4, largest]
+    # h = np.histogram(energies, bins=20)
+    mask = np.ma.array(largest, mask=False)
+    # print(mask)
+    for i in range(largest.shape[0]):
+        # filter out events that didn't reconstruct
+        if int(data_t[1, largest[i]]) == -1 or data_t[21, largest[i]] == 0.0:
+            mask[i] = np.ma.masked
+        elif energies[i] > 10000:
+            get_useful_info(data[largest[i]])
+            # mask[i] = np.ma.masked
+    largest = mask.compressed()
+    # print(largest)
+    energies = data_t[4, largest]
+    # print(energies)
+    # print(np.count_nonzero(energies == 0.192373))
+    l = data_t[5, largest]
+    b = data_t[6, largest]
+    # plt.hist(energies, bins=200)
+    # plt.show()
+    return energies, l, b
+
+
+def process_sim_output(fname):
+    data = np.genfromtxt(fname, dtype=np.float64)
+    energies = data[:, 0]
+    pos = data[:, 1:4]
+    print(pos.shape)
+    ls = np.zeros(data.shape[0])
+    bs = np.zeros(data.shape[0])
+    for i in range(data.shape[0]):
+        x, y, z = pos[i]
+        vec = Vector3d(x, y, z)
+        ls[i] = vec.getPhi()*180.0/np.pi
+        bs[i] = vec.getTheta()*180.0/np.pi
+    return energies, ls, bs
+
+def generate_fake_data(n):
+    i = 0
+    energies = np.zeros(n)
+    gal_l = np.zeros(n)
+    gal_b = np.zeros(n)
+    for i in range(n):
+        energies[i] = np.power(10, 21-np.log10(1000-999*random())-18)
+        gal_l[i] = random()*360.0-180.0
+        gal_b[i] = random()*180.0-90.0
+        i += 1
+    return energies, gal_l, gal_b
+
+
+# energies, l, b = process_raw_auger('/home/sam/Downloads/auger-data/Archive', '\s+', 7)
+# energies, l, b = process_sim_output('samples/evt_0_a.txt')
+energies, l, b = generate_fake_data(10)
+for data in zip(energies, l, b):
+    print(data[0],data[1], data[2])
 
 fig = plt.figure(figsize=(12, 9), edgecolor='w')
 plt.subplot('211')
-m = Basemap(projection='eck4', lon_0=-80)
-# m.drawcoastlines()
-
+# plt.hist(energies2, bins=40)
+m = Basemap(projection='hammer', lon_0=-80)
+# print(energies)
 # lat, lon = 29.7630556,-95.3630556
 # x, y = m(lon, lat)
 x, y = m(l, b)
@@ -46,4 +136,6 @@ m.colorbar()
 
 plt.subplot('212')
 plt.hist(energies, bins=40)
+# hist, bins = np.histogram(energies, bins=20)
+# plt.loglog(bins[1:], hist, marker='o', linestyle='None')
 plt.show()
